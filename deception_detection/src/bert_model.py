609ed3a90e4db05b692553da4c49401e1458f174
Dana <danaeskazi@gmail.com>
2026-03-26 13:21:14 +0500

    update bert

--- deception_detection/src/bert_model.py
+++ deception_detection/src/bert_model.py
@@ -126,13 +126,13 @@ def train_bert_model(train_texts, test_texts, train_labels, test_labels):
     training_args = TrainingArguments(
         output_dir="./bert_results",
 
-        # IMPORTANT FIX: correct parameter name
+
         eval_strategy="epoch",
 
-        save_strategy="no",
+        save_strategy="epoch",           
         learning_rate=2e-5,
         per_device_train_batch_size=16,
-        per_device_eval_batch_size=8,
+        per_device_eval_batch_size=16,
         num_train_epochs=5,
         weight_decay=0.01,
         warmup_ratio=0.1,                
